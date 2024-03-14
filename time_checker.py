8e4c31c2a668182476ab984fb1aef48fd10cd292
SergeySysoev <sergey.sysoev3010@gmail.com>
2024-03-14 17:43:25 +0300

    updated data

--- time_checker.py
+++ time_checker.py
@@ -39,7 +39,7 @@ def frequency_based_scipy(image):
 
 @time_it
 @nb.njit
-def frequncy_based_numba(image):
+def frequnecy_based_numba(image):
     f_transform = np.fft.fft2(image)
     f_transform_shifted = np.fft.fftshift(f_transform)
     magnitude_spectrum = np.abs(f_transform_shifted)
@@ -49,50 +49,51 @@ def edge_based(image):
     edges = cv2.Canny(image, 50, 150)
     return np.sum(edges) / float(image.size)
 
-df = pd.read_csv('processed_analyzed.csv')
 
-times = []
-lap = []
-grad = []
-freq = []
-edge = []
-sp_freq = []
-resize = []
-numba = []
+if __name__ == '__main__':
+    df = pd.read_csv('processed_analyzed.csv')
 
-for i, img in enumerate(df['path']):
-	# if i == 1000:
-	# 	break
-	bwimg = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-	# print('File:', img, 'exists:', os.path.exists(img))
-	start = time.time()
-	bwimg = cv2.resize(bwimg, (640, 640))
-	end = time.time()
-	resize.append(end - start)
-	# start = time.time()
-	# laplacian_variance(bwimg)
-	# end_lap = time.time()
-	# gradient_magnitude(bwimg)
-	pr_n, t_numpy = frequency_based(bwimg)
-	pr_s, t_scipy = frequency_based_scipy(bwimg)
-	pr_numba, t_numba = frequncy_based_numba(bwimg)
+    times = []
+    lap = []
+    grad = []
+    freq = []
+    edge = []
+    sp_freq = []
+    resize = []
+    numba = []
 
-	# edge_based(bwimg)
-	# end = time.time()
-	# times.append(end - start)
-	# lap.append(end_lap - start)
-	# grad.append(end_grad - end_lap)
-	freq.append(t_numpy)
-	# edge.append(end - end_freq)
-	sp_freq.append(t_scipy)
-	numba.append(t_numba)
+    for i, img in enumerate(df['path']):
+        # if i == 1000:
+        # 	break
+        bwimg = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+        # print('File:', img, 'exists:', os.path.exists(img))
+        start = time.time()
+        bwimg = cv2.resize(bwimg, (640, 640))
+        end = time.time()
+        resize.append(end - start)
+        # start = time.time()
+        # laplacian_variance(bwimg)
+        # end_lap = time.time()
+        # gradient_magnitude(bwimg)
+        pr_n, t_numpy = frequency_based(bwimg)
+        pr_s, t_scipy = frequency_based_scipy(bwimg)
+        pr_numba, t_numba = frequnecy_based_numba(bwimg)
+        # edge_based(bwimg)
+        # end = time.time()
+        # times.append(end - start)
+        # lap.append(end_lap - start)
+        # grad.append(end_grad - end_lap)
+        freq.append(t_numpy)
+        # edge.append(end - end_freq)
+        sp_freq.append(t_scipy)
+        numba.append(t_numba)
 
-# print(f'laplacian_variance mean: {1000 * sum(lap)/len(lap)}')
-#print(f'gradient_magnitude mean: {1000 * sum(grad)/len(grad)}')
-print(f'numba frequency_based mean: {1000 * sum(numba)/len(numba)}')
-print(f'numpy frequency_based mean: {1000 * sum(freq)/len(freq)}')
-print(f'scipy frequency_based mean: {1000 * sum(sp_freq)/len(sp_freq)}')
-print(f'resize mean: {1000 * sum(resize)/len(resize)}')
+    # print(f'laplacian_variance mean: {1000 * sum(lap)/len(lap)}')
+    #print(f'gradient_magnitude mean: {1000 * sum(grad)/len(grad)}')
+    print(f'numba frequency_based mean: {1000 * sum(numba)/len(numba)}')
+    print(f'numpy frequency_based mean: {1000 * sum(freq)/len(freq)}')
+    print(f'scipy frequency_based mean: {1000 * sum(sp_freq)/len(sp_freq)}')
+    print(f'resize mean: {1000 * sum(resize)/len(resize)}')
 
 #print(f'edge_based mean: {1000 * sum(edge)/len(edge)}')
 #print(f"Mean time: {(sum(times)/len(times))* 1000}")
